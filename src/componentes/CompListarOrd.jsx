f365f592a63fb91831b3f6a0cddb325bad2b2669
Carlos Borrero <borreroca@uninorte.edu.co>
2021-12-05 13:26:22 -0500

    "

--- src/componentes/CompListarOrd.jsx
+++ src/componentes/CompListarOrd.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react"
 import rutas from "./rutas"
 import "./ListarOrdenes.css"
+import { OrdenDespacho } from '../datos'
 
 const ListarOrdenes = () => {
+
+    const ordenes=OrdenDespacho
+
+    const [listaOrdenes, setListaOrdenes] = useState(ordenes);
+
+    const buscaOrdenes = (evento) => {
+        setListaOrdenes(        
+            ordenes.filter(o => o.estado.toLowerCase().includes
+            (evento.target.value.toLowerCase())))
+    };
+
     return(
         <div>
             <div className="row fluid" >
                 <div className="container-fluid d-flex justify-content-center align-items-center">
                     <h1 className="col-11 text-center">Mi historial de ordenes de despacho</h1>
-                    <select class="form-select m-2" aria-label="Default select example">
-                        <option selected>Todos</option>
-                        <option value="1">Pendiente</option>
-                        <option value="2">Despachada</option>
-                        <option value="3">Cancelada</option>
-                        <option value="3">Finalizada</option>
+                    <select class="form-select m-2" aria-label="Default select example" onChange={buscaOrdenes}>
+                        <option selected value="">Todos</option>
+                        <option value="Pendiente">Pendiente</option>
+                        <option value="Despachado">Despachado</option>
+                        <option value="Cancelado">Cancelado</option>
+                        <option value="Finalizado">Finalizado</option>
                     </select>
                 </div>
             </div>
@@ -24,19 +36,19 @@ const ListarOrdenes = () => {
                         <tr>
                             <th scope="col">No. Orden</th>
                             <th scope="col"></th>
-                            <th scope="col">Fecha</th>
+                            <th scope="col">Precio</th>
                             <th scope="col"></th>
                             <th scope="col">Estado</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {rutas.map((r)=>
+                        {listaOrdenes.map((o)=>
                             <tr style={{ 'background-color': '#fff'}}>
-                                <td>1</td>
+                                <td>{o.codigo}</td>
                                 <td>|</td>
-                                <td>20/11/2021</td>
+                                <td>{o.precio}</td>
                                 <td>|</td>
-                                <td>Pendiente</td>
+                                <td>{o.estado}</td>
                             </tr>
                         )}                        
                     </tbody>
