e4ca5c2ea91762c9569495209c0ea298f699e51a
oscar novoa <novthel@live.com>
2021-12-07 15:55:42 -0500

    se elimino pagina OrdenDesp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListarOrdenesDesp from './pages/ListarOrdenesDesp';
 import ListarOrdenesPage from "./pages/ListarOrdenes"
 import EditarRutasPage from "./pages/EditarRutas"
-import OrdenDesp from './pages/OrdenDesp';
 import Registro from './pages/Registro';
 import Login from './pages/Login';
 import Quienessomos from './pages/Quienessomos';
@@ -22,7 +21,6 @@ function App() {
         <Route path="/Login" element= {<Login/>}/>
         <Route path="/" element= {<Home/>}/>
         <Route path='/listarODD' element={<ListarOrdenesDesp/>}/>
-        <Route path='/OrdenDesp' element={<OrdenDesp/>}/>
         <Route path="/ListarOrdenes" element={<ListarOrdenesPage />} />
         <Route path="/EditarRutas" element={<EditarRutasPage />} />
       </Routes>
--- src/pages/ListarOrdenesDesp.js
+++ src/pages/ListarOrdenesDesp.js
@@ -1,42 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuAdmin from '../componentes/MenuAdmin';
 import Cabecera from '../componentes/Cabecera';
 import Pie from '../componentes/Pie';
-
+import Input from '../componentes/Input';
 import Button from '../componentes/Button';
 import { OrdenDespacho } from '../datos';
 
-let user= {codigo:'',cliente:'',precio:'', descripcion:'', camiones:'',origen:'',destino:'', nombreProd:'',peso:''};
 
 const ListarOrdenesDesp = () => {
     const orden = OrdenDespacho
-
+    const [code,setCode] =useState("");    
+    const [user,setUser] =useState({});
+    const [modalState, setModalState] = useState(false);    
     
+    const ejecutarModal = (event) => { 
+        event.preventDefault();
+        var item = OrdenDespacho.find(item => item.codigo === parseInt(code));
+            if (item!==undefined) {
+                const userr = {
+                    codigo:item.codigo,
+                    cliente:item.cliente,
+                    precio:item.precio,
+                    descripcion: item.descripcion,
+                    camiones:item.camiones,
+                    origen:item.origen,
+                    destino:item.destino,
+                    nombreProd:item.nombreProd,
+                    peso:item.peso
 
-    const ModalODD = (codigo,cliente,precio, descripcion, camiones,origen,destino, nombreProd,peso) => {
-    
-        user.codigo=codigo;
-        user.cliente=cliente;
-        user.precio=precio;
-        user.descripcion= descripcion;
-        user.camiones=camiones;
-        user.origen=origen;
-        user.destino=destino;
-        user.nombeProd=nombreProd;
-        user.peso=peso;
-
-        console.log('imprimir')
-    }
-        
-            
+                }
+                setUser(userr);
+                setModalState(true);
+            } else{
+                setModalState(false);
+                alert("Dato invalido");
+                console.log(document.getElementsByClassName("modal-backdrop fade show")[0].style.display = "none");
+            }
+  
+    }   
       
     return (
         
         <>
             <Cabecera/>
             <MenuAdmin/>
-            
-
             <div>
                 <h5>Ordenes despachadas en el Dia</h5>
             </div>
@@ -56,84 +63,91 @@ const ListarOrdenesDesp = () => {
                                                 <td>{p.codigo}</td>
                                                 <td>{p.cliente}</td>
                                                 <td>{p.precio}</td>
-                                                <td style={{ 'background-color':'#e7e7de' }}><Button type="button" className={p.codigo} data-bs-toggle="modal"
-                                                data-bs-target="#modalODD" onClick={ModalODD(p.codigo,p.cliente,p.precio, p.descripcion, p.camiones,p.origen,p.destino, p.nombreProd, p.peso )}/></td>
                                             </tr>)}
-                        
                     </tbody>
-                </table>  
+                </table>
+                <Input
+                            type="text"
+                            name="consulta"
+                            id="consulta"
+                            value="Consulta"
+                            required="required"
+                            setState={setCode}
+                            state={code}
+                        />
+                <Button type="button" className="" data-bs-toggle="modal"
+                  data-bs-target="#modalODD" onClick={ejecutarModal}/>
                 <div className='vistaorden'>
                 <div>
-        
-        <div className="modal fade" id="modalODD" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div className="modal-dialog modal-lg" >
-                <div className="modal-content" style={{'background-color': 'LightGray'}}>
-                    <div class="modal-body">
-                    
-                        <div className="card-body">
-                            <h4 className="card-title">Orden de Despacho No.{user.codigo}</h4><br/>
 
-                            <div className="container">
-                                <div className="row">  
-                                    <h6 className="card-subtitle mb-2">Informacion de Producto(s) a enviar</h6> <br/>    
-                                    
-                                    <div className="col-5" style={{'background': 'white', 'margin-left':'1%'}}>
-                                        <table className="table table-sm">
-                                            <tbody>
-                                                <tr className='thead'>
-                                                    <th scope="row">Nombre:</th>
-                                                    <td>{user.nombreProd}</td>
-                                                </tr> 
-                                                <br/>
-                                                <tr className='thead'>
-                                                    <th scope="row">Descripcion:</th> 
-                                                    <td>{user.descripcion}</td>
-                                                </tr> 
-                                                <br/>
-                                                <tr className='thead'>
-                                                    <th scope="row">Peso:</th>
-                                                    <td>{user.peso}</td>
-                                                </tr> 
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                
-                                    <div className="col-6" style={{'margin-left':'6%'}}>
-                                        <table className="table table-sm">
-                                            <tbody>
-                                                <tr className='thead'>
-                                                    <th scope="row">Cliente:</th>
-                                                    <td>{user.cliente}</td>
-                                                </tr> <br/>
-                                                <tr className='thead'>
-                                                    <th scope="row">Ruta:</th>
-                                                    <td>{user.origen}-{user.destino}</td>
-                                                </tr> <br/>
-                                                <tr className='thead'>
-                                                    <th scope="row">Cantidad de Camiones:</th>
-                                                    <td>{user.camiones}</td>
-                                                </tr> <br/> <br/>
-                                                <tr className='thead'>
-                                                    <th scope="row">Total a Pagar:</th>
-                                                    <td>$ {user.precio}</td>
-                                                </tr>
-                                            </tbody>
+                    <div className="modal fade" id="modalODD" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" hidden  ={!modalState}>        
+                    <div className="modal-dialog modal-lg" >
+                        <div className="modal-content" style={{'background-color': 'LightGray'}}>
+                            <div class="modal-body">
+                            
+                                <div className="card-body">
+                                    <h4 className="card-title">Orden de Despacho No.{user.codigo}</h4><br/>
 
-                                        </table>
-                                    </div>
+                                    <div className="container">
+                                        <div className="row">  
+                                            <h6 className="card-subtitle mb-2">Informacion de Producto(s) a enviar</h6> <br/>    
+                                            
+                                            <div className="col-5" style={{'background': 'white', 'margin-left':'1%'}}>
+                                                <table className="table table-sm">
+                                                    <tbody>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Nombre:</th>
+                                                            <td>{user.nombreProd}</td>
+                                                        </tr> 
+                                                        <br/>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Descripcion:</th> 
+                                                            <td>{user.descripcion}</td>
+                                                        </tr> 
+                                                        <br/>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Peso:</th>
+                                                            <td>{user.peso}</td>
+                                                        </tr> 
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        
+                                            <div className="col-6" style={{'margin-left':'6%'}}>
+                                                <table className="table table-sm">
+                                                    <tbody>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Cliente:</th>
+                                                            <td>{user.cliente}</td>
+                                                        </tr> <br/>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Ruta:</th>
+                                                            <td>{user.origen}-{user.destino}</td>
+                                                        </tr> <br/>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Cantidad de Camiones:</th>
+                                                            <td>{user.camiones}</td>
+                                                        </tr> <br/> <br/>
+                                                        <tr className='thead'>
+                                                            <th scope="row">Total a Pagar:</th>
+                                                            <td>$ {user.precio}</td>
+                                                        </tr>
+                                                    </tbody>
 
-                                </div>
-                            </div>        
-                        </div>                
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                </table>
+                                            </div>
+
+                                        </div>
+                                    </div>        
+                                </div>                
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-    </div>
 
                 </div>  
             </div>
@@ -144,6 +158,3 @@ const ListarOrdenesDesp = () => {
 }
 
 export default ListarOrdenesDesp
-
-
-
